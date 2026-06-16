d1ad932aa12626ade2dd7d7e55a10a78ac48f115
regisseurai <regisseurai@gmail.com>
2026-06-16 01:43:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Caches the site shell so the app installs and opens instantly.
    Videos and Spotify always stream live from the network. */
 
-const CACHE = 'regx-v2';
+const CACHE = 'regx-v3';
 const SHELL = [
   './',
   './index.html',
